8c5dfa4e8b2904da3442169f1b1647d4aa7ae15a
Dangelot2020 <72313354+Dangelot2020@users.noreply.github.com>
2020-10-03 18:38:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>TEST E-MAIL</title>
+        <link rel="stylesheet" href="css/matel.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <!-- Optional theme -->
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper">
+            <div class="content-wrapper">
+                    <!-- Main content -->
+                    <section class="content">
+                        <div class="container-fluid">
+                            <form action="#" method="POST" autocomplete="off">
+                                <div class="form-group col-md-6">
+                                    <label for="">E-mail</label>
+                                    <input type="text" class="form-control" name="n_mail" value="" autofocus />
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="">Titre message</label>
+                                    <input type="text" class="form-control" name="n_titre" value="" autofocus />
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label for="">Message</label>
+                                    <textarea name="n_message" id="n_message" length="2" cols="30" rows="10" class="form-control"></textarea>
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <button class="btn btn-primary" name="btn_valider">Valider</button>
+                                </div>
+                            </form>
+                        </div>
+                    </section>
+            </div>
+        </div>
+    </body>
+</html>
